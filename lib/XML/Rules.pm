0947825dc976b193cc27d8661d4a2398a67d643e
Jan Krynický <Jenda@Krynicky.cz>
2008-02-27 00:28:34 +0000

    Import of JENDA/XML-Rules-1.01 from CPAN.
    gitpan-cpan-distribution: XML-Rules
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         JENDA/XML-Rules-1.01.tar.gz
    gitpan-cpan-author:       JENDA
    gitpan-cpan-maturity:     released

--- lib/XML/Rules.pm
+++ lib/XML/Rules.pm
@@ -17,11 +17,11 @@ XML::Rules - parse XML & process tags by rules starting from leaves
 
 =head1 VERSION
 
-Version 1.00
+Version 1.01
 
 =cut
 
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 
 =head1 SYNOPSIS
 
